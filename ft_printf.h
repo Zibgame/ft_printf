afe3572026aaf18febb4018b282fdb8f586b289f
zibgame <zcadinot@c2r8p1.42lehavre.fr>
2025-10-29 12:25:06 +0100

    42

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: zcadinot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 11:34:50 by zcadinot          #+#    #+#             */
-/*   Updated: 2025/10/22 12:54:30 by zcadinot         ###   ########.fr       */
+/*   Updated: 2025/10/29 11:00:06 by zcadinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,19 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-int	ft_printf(const char *s, ...);
-int	ft_putarg_char(char type, va_list args);
-int	ft_putarg_int(char type, va_list args);
-int	parser(const char *str, va_list args);
-int	ft_putarg_hex(char type, va_list args);
-int	ft_putptr(void *ptr);
-int	ft_putptr_recursive(unsigned long n);
-int	ft_putarg_hex(char type, va_list args);
-int	ft_puthex_recursive(unsigned int n, char *base);
-int	ft_putnbr_fd(int n, int fd);
-int	ft_putunbr_fd(unsigned int n, int fd);
 size_t	ft_strlen(const char *str);
+int		ft_printf(const char *s, ...);
+int		ft_putarg_char(char type, va_list args);
+int		ft_putarg_int(char type, va_list args);
+int		parser(const char *str, va_list args);
+int		ft_putarg_hex(char type, va_list args);
+int		ft_putptr(void *ptr);
+int		ft_putptr_recursive(unsigned long n);
+int		ft_putarg_hex(char type, va_list args);
+int		ft_puthex_recursive(unsigned int n, char *base);
+int		ft_putnbr_fd(int n, int fd);
+int		ft_putunbr_fd(unsigned int n, int fd);
+/* int		main(void); */
 void	ft_putchar_fd(char c, int fd);
-int	main(void);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: zcadinot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 12:04:22 by zcadinot          #+#    #+#             */
-/*   Updated: 2025/10/28 12:04:24 by zcadinot         ###   ########.fr       */
+/*   Updated: 2025/10/29 12:17:45 by zcadinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,7 @@ int	main(void)
 	a = 255;
 	b = 3735928559;
 	ptr = &a;
-	printf("=== TEST %%x (hex minuscule) ===\n");
-	printf(NULL);
-	ft_printf(NULL);
+	ft_printf("%d ->", printf("%%\t%c\t%s\t%p\t%p\t%d\n%i\t%u\t%x\t%X\n\n", 'a', "aaa", &a, ptr, 0, -42, -1, 16, 16));
+	ft_printf("%d ->", ft_printf("%%\t%c\t%s\t%p\t%p\t%d\n%i\t%u\t%x\t%X\n\n", 'a', "aaa", &a, ptr, 0, -42, -1, 16, 16));
 	return (0);
 }
